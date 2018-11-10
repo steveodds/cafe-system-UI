c25f298331147fee7094c0378578517f0b751659
Steve Odds <steveremdi@gmail.com>
2018-11-10 19:13:56 +0300

    Finalized form additions and UI tweaks.

--- Cafeteria System/Cafeteria System/View_Order.xaml.cs
+++ Cafeteria System/Cafeteria System/View_Order.xaml.cs
@@ -10,37 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Cafeteria_System
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for View_Order.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class View_Order : Window
     {
-        public MainWindow()
+        public View_Order()
         {
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            New_Order new_Order = new New_Order();
-            new_Order.Show();
-            this.Close();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            View_Order view_Order = new View_Order();
-            view_Order.Show();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
             this.Close();
         }
     }
